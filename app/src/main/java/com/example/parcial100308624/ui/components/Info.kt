e6676e5657eff1ef7f61c4f91a6e55bae6b39ff4
TuNombre <TuCorreo@uca.edu.sv>
2026-04-28 09:50:07 -0600

    Agregar archivo Info.kt

--- app/src/main/java/com/example/parcial100308624/ui/components/Info.kt
+++ app/src/main/java/com/example/parcial100308624/ui/components/Info.kt
@@ -1,2 +1,46 @@
 package com.example.parcial100308624.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Info(
+    title: String,
+    value: String
+) {
+    Box(
+        modifier = Modifier
+            .padding(8.dp)
+            .background(
+                color = Color(0xFF1C1F2E),
+                shape = RoundedCornerShape(16.dp)
+            )
+            .padding(16.dp)
+    ) {
+        Column {
+            Icon(
+                imageVector = Icons.Default.CheckCircle,
+                contentDescription = null,
+                tint = Color.White
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = title,
+                color = Color.Gray
+            )
+            Text(
+                text = value,
+                color = Color.White
+            )
+        }
+    }
+}
