74753e213daa11aa12246a4ce7f149762692b091
TuNombre <TuCorreo@uca.edu.sv>
2026-04-28 09:53:37 -0600

    Agregar archivo Transaction.kt

--- app/src/main/java/com/example/parcial100308624/ui/components/Transaction.kt
+++ app/src/main/java/com/example/parcial100308624/ui/components/Transaction.kt
@@ -1,2 +1,47 @@
 package com.example.parcial100308624.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Transaction(
+    title: String,
+    date: String,
+    amount: String,
+    isPositive: Boolean = true
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 6.dp)
+            .background(
+                color = Color(0xFF1C1F2E),
+                shape = RoundedCornerShape(12.dp)
+            )
+            .padding(12.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column {
+            Text(
+                text = title,
+                color = Color.White
+            )
+            Text(
+                text = date,
+                color = Color.Gray
+            )
+        }
+        Text(
+            text = amount,
+            color = if (isPositive) Color(0xFF00C853) else Color.Red
+        )
+    }
+}
