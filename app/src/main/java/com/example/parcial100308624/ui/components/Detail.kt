e30ba272cf5c149b100dbfef209056d3884e4c57
TuNombre <TuCorreo@uca.edu.sv>
2026-04-28 09:47:08 -0600

    Agregar archivo Detail.kt

--- app/src/main/java/com/example/parcial100308624/ui/components/Detail.kt
+++ app/src/main/java/com/example/parcial100308624/ui/components/Detail.kt
@@ -1,2 +1,37 @@
 package com.example.parcial100308624.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Detail(
+    label: String,
+    value: String
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 6.dp)
+            .background(
+                color = Color(0xFF1C1F2E),
+                shape = RoundedCornerShape(12.dp)
+            )
+            .padding(12.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = label,
+            color = Color.Gray
+        )
+        Text(
+            text = value,
+            color = Color.White
+        )
+    }
+}
